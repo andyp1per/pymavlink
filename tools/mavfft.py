40a07f29255cb6d8d0e75a9233432ad2fe50fccd
Andy Piper <github@andypiper.com>
2020-05-07 10:23:15 +0100

    mavfft: process raw gyro data

--- tools/mavfft.py
+++ tools/mavfft.py
@@ -24,16 +24,9 @@ def fft(logfile):
     print("Processing log %s" % filename)
     mlog = mavutil.mavlink_connection(filename)
 
-    data = {'ACC1.rate' : 1000,
-            'ACC2.rate' : 1600,
-            'ACC3.rate' : 1000,
-            'GYR1.rate' : 1000,
-            'GYR2.rate' :  800,
-            'GYR3.rate' : 1000}
-    for acc in ['ACC1','ACC2','ACC3']:
-        for ax in ['AccX', 'AccY', 'AccZ']:
-            data[acc+'.'+ax] = []
-    for gyr in ['GYR1','GYR2','GYR3']:
+    data = {'FTN3.rate' : 1000,
+            'GYR1.rate' : 1000 }
+    for gyr in ['FTN3','GYR1']:
         for ax in ['GyrX', 'GyrY', 'GyrZ']:
             data[gyr+'.'+ax] = []
 
@@ -43,22 +36,22 @@ def fft(logfile):
         if m is None:
             break
         type = m.get_type()
-        if type.startswith("ACC"):
-            data[type+'.AccX'].append(m.AccX)
-            data[type+'.AccY'].append(m.AccY)
-            data[type+'.AccZ'].append(m.AccZ)
-        if type.startswith("GYR"):
+        if type.startswith("GYR1"):
+            data[type+'.GyrX'].append(m.GyrX)
+            data[type+'.GyrY'].append(m.GyrY)
+            data[type+'.GyrZ'].append(m.GyrZ)
+        if type.startswith("FTN3"):
             data[type+'.GyrX'].append(m.GyrX)
             data[type+'.GyrY'].append(m.GyrY)
             data[type+'.GyrZ'].append(m.GyrZ)
 
-    print("Extracted %u data points" % len(data['ACC1.AccX']))
+    print("Extracted %u data points" % len(data['FTN3.GyrX']))
 
-    for msg in ['ACC1', 'ACC2', 'ACC3', 'GYR1', 'GYR2', 'GYR3']:
+    for msg in ['FTN3', 'GYR1']:
         pylab.figure()
 
-        if msg.startswith('ACC'):
-            prefix = 'Acc'
+        if msg.startswith('FTN'):
+            prefix = 'Gyr'
         else:
             prefix = 'Gyr'
         for axis in ['X', 'Y', 'Z']:
